2a298ec7189d59f2eabe0296a1ca7008cb419ef1
David Hoppe <hopasaurus@gmail.com>
2019-11-23 19:26:36 -0500

    Added helper to test to remove clutter and help them be more readable.

--- test/redis-chunker.test.js
+++ test/redis-chunker.test.js
@@ -15,119 +15,75 @@ describe('redis-chunker', function () {
     });
 
     it('calls callback with string', function () {
-        const testData = Buffer.from('+test\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from('+test\r\n')]);
+        const result = processTestData([Buffer.from('+test\r\n')]);
+        assert.deepEqual(result, [Buffer.from('+test\r\n')]);
     });
 
-    it('does not call callback with partial string', function () {
-        const testData = Buffer.from('+test\r');
+    const processTestData = (data) => {
         const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.equal(callbackData.length, 0);
+        const chunker = new Chunker(chunk => callbackData.push(chunk));
+        data.forEach(d => chunker.data(d));
+        return callbackData;
+    };
+
+    it('does not call callback with partial string', function () {
+        const result = processTestData([Buffer.from('+test\r')]);
+        assert.equal(result.length, 0);
     });
 
     it('calls callback with string spread across multiple data calls', function () {
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(Buffer.from('+test\r'));
-        chunker.data(Buffer.from('\n'));
-        assert.deepEqual(callbackData, [Buffer.from('+test\r\n')]);
+        const result = processTestData([Buffer.from('+test\r'), Buffer.from('\n')]);
+        assert.deepEqual(result, [Buffer.from('+test\r\n')]);
     });
 
     it('calls callback multiple times for multiple strings', function () {
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(Buffer.from('+test\r\n'));
-        chunker.data(Buffer.from('+test2\r\n'));
-        assert.deepEqual(callbackData, [Buffer.from('+test\r\n'), Buffer.from('+test2\r\n')]);
+        const result = processTestData([Buffer.from('+test\r\n'), Buffer.from('+test2\r\n')]);
+        assert.deepEqual(result, [Buffer.from('+test\r\n'), Buffer.from('+test2\r\n')]);
     });
 
     it('calls callback multiple times for multiple strings sent in a single chunk', function () {
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(Buffer.from('+test\r\n+test2\r\n'));
-        assert.deepEqual(callbackData, [Buffer.from('+test\r\n'), Buffer.from('+test2\r\n')]);
+        const result = processTestData([Buffer.from('+test\r\n+test2\r\n')]);
+        assert.deepEqual(result, [Buffer.from('+test\r\n'), Buffer.from('+test2\r\n')]);
     });
 
     it('calls callback for null string (bulk string)', function () {
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(Buffer.from('$-1\r\n'));
-        assert.deepEqual(callbackData, [Buffer.from('$-1\r\n')]);
+        const result = processTestData([Buffer.from('$-1\r\n')]);
+        assert.deepEqual(result, [Buffer.from('$-1\r\n')]);
     });
 
     it('calls callback for empty bulk string', function () {
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(Buffer.from('$0\r\n\r\n'));
-        assert.deepEqual(callbackData, [Buffer.from('$0\r\n\r\n')]);
+        const result = processTestData([Buffer.from('$0\r\n\r\n')]);
+        assert.deepEqual(result, [Buffer.from('$0\r\n\r\n')]);
     });
 
     it('calls callback with an error type message', function () {
-        const testData = Buffer.from('-test\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from('-test\r\n')]);
+        const result = processTestData([Buffer.from('-test\r\n')]);
+        assert.deepEqual(result, [Buffer.from('-test\r\n')]);
     });
 
     it('calls callback with an integer type message', function () {
-        const testData = Buffer.from(':1234\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from(':1234\r\n')]);
+        const result = processTestData([Buffer.from(':1234\r\n')]);
+        assert.deepEqual(result, [Buffer.from(':1234\r\n')]);
     });
 
     it('calls callback with a null array', function () {
-        const testData = Buffer.from('*-1\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from('*-1\r\n')]);
+        const result = processTestData([Buffer.from('*-1\r\n')]);
+        assert.deepEqual(result, [Buffer.from('*-1\r\n')]);
     });
 
     it('calls callback with an empty array', function () {
-        const testData = Buffer.from('*0\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from('*0\r\n')]);
+        const result = processTestData([Buffer.from('*0\r\n')]);
+        assert.deepEqual(result, [Buffer.from('*0\r\n')]);
     });
 
     it('calls callback with an array with a single string', function () {
-        const testData = Buffer.from('*1\r\n+foo\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from('*1\r\n+foo\r\n')]);
+        const result = processTestData([Buffer.from('*1\r\n+foo\r\n')]);
+        assert.deepEqual(result, [Buffer.from('*1\r\n+foo\r\n')]);
     });
 
     it('calls callback with an array with a single string spread over two messages', function () {
-        const testData = Buffer.from('*1\r\n');
-        const testData2 = Buffer.from('+foo\r\n');
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        chunker.data(testData2);
-        assert.deepEqual(callbackData, [Buffer.from('*1\r\n+foo\r\n')]);
+        const result = processTestData([Buffer.from('*1\r\n+foo\r\n')]);
+        assert.deepEqual(result, [Buffer.from('*1\r\n+foo\r\n')]);
     });
 
     it('calls callback with an array with embedded array', function () {
@@ -139,12 +95,7 @@ describe('redis-chunker', function () {
             '*2\r\n' +
             '+Foo\r\n' +
             '-Bar\r\n';
-        const testData = Buffer.from(arrayWithArrayEmbedded);
-        const callbackData = [];
-        const callback = chunk => callbackData.push(chunk);
-        const chunker = new Chunker(callback);
-        chunker.data(testData);
-        assert.deepEqual(callbackData, [Buffer.from(arrayWithArrayEmbedded)]);
+        const result = processTestData([Buffer.from(arrayWithArrayEmbedded)]);
+        assert.deepEqual(result, [Buffer.from(arrayWithArrayEmbedded)]);
     });
-
 });
